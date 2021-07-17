f4130552d4875526975ca0e87ac1f5d841a88ba9
Rafael Schwantes <rafaelschwantes@gmail>
2021-07-17 14:06:50 +0100

    alteracao path imagem

--- pages/login.js
+++ pages/login.js
@@ -11,7 +11,7 @@ export default function LoginScreen() {
     <main style={{ display: 'flex', flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <div className="loginScreen">
         <section className="logoArea">
-          <img src="https://alurakut.vercel.app/logo-schworkut.svg" />
+          <img src="https://schworkut-rafaelschwantes.vercel.app/logo-schworkut.svg" />
 
           <p><strong>Conecte-se</strong> aos seus amigos e familiares usando recados e mensagens instantâneas</p>
           <p><strong>Conheça</strong> novas pessoas através de amigos de seus amigos e comunidades</p>
